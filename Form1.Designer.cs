f47dcb685ae007606d326d0f029aa9cc78b6212a
Mateusz Dobkowski <mateusz.dobkoski@gmail.com>
2022-02-02 11:39:34 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace SyntezaMowy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelVoice = new System.Windows.Forms.Label();
+            this.comboBoxVoice = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.btnHigh = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelVoice
+            // 
+            this.labelVoice.AutoSize = true;
+            this.labelVoice.Location = new System.Drawing.Point(12, 19);
+            this.labelVoice.Name = "labelVoice";
+            this.labelVoice.Size = new System.Drawing.Size(30, 13);
+            this.labelVoice.TabIndex = 3;
+            this.labelVoice.Text = "Głos";
+            // 
+            // comboBoxVoice
+            // 
+            this.comboBoxVoice.FormattingEnabled = true;
+            this.comboBoxVoice.Location = new System.Drawing.Point(48, 16);
+            this.comboBoxVoice.Name = "comboBoxVoice";
+            this.comboBoxVoice.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxVoice.TabIndex = 4;
+            this.comboBoxVoice.Text = "Wybierz glos";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 407);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(174, 407);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 8;
+            this.btnPause.Text = "Pauza";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(336, 407);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Zapisz";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 407);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 10;
+            this.btnLoad.Text = "Wczytaj";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(255, 407);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 11;
+            this.btnResume.Text = "Wznów";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 358);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(93, 448);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(600, 95);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Szybkość";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Szybko";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Normalnie";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Powoli";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 19);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(588, 45);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.trackBar2);
+            this.groupBox2.Location = new System.Drawing.Point(93, 549);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(600, 95);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Głośność";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(552, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Głośno";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Normalnie";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Cicho";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(6, 19);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(588, 45);
+            this.trackBar2.TabIndex = 0;
+            this.trackBar2.Value = 50;
+            // 
+            // btnHigh
+            // 
+            this.btnHigh.Location = new System.Drawing.Point(417, 407);
+            this.btnHigh.Name = "btnHigh";
+            this.btnHigh.Size = new System.Drawing.Size(119, 23);
+            this.btnHigh.TabIndex = 16;
+            this.btnHigh.Text = "Czytaj zaznaczone";
+            this.btnHigh.UseVisualStyleBackColor = true;
+            this.btnHigh.Click += new System.EventHandler(this.btnHigh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 675);
+            this.Controls.Add(this.btnHigh);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBoxVoice);
+            this.Controls.Add(this.labelVoice);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelVoice;
+        private System.Windows.Forms.ComboBox comboBoxVoice;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button btnHigh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+using GemBox.Pdf;
+using Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+
+namespace SyntezaMowy
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer voice;
+        bool ssml = false;
+        private readonly List<System.Drawing.Point> theWordsLocations = new List<System.Drawing.Point>();
+        private int loc = 0;
+        private List<string> theWords = new List<string>();
+
+        private readonly Color HighLightBackColor = Color.Yellow;
+        private Color previousSelectionBackColor;
+
+        private System.Drawing.Point previousSelection;
+
+        private int currentWord;
+        private int nWords;
+
+        private readonly char[] splitChar = { ' ', '\n' };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+       public void speakCompleted(object sender, SpeakCompletedEventArgs e)
+       {
+           richTextBox1.SelectAll();
+           richTextBox1.SelectionBackColor = previousSelectionBackColor;
+       }
+
+       private void voice_SpeakProgress(object sender, SpeakProgressEventArgs e)
+       {
+           if (currentWord == theWordsLocations.Count)
+           {
+               richTextBox1.SelectionBackColor = previousSelectionBackColor;
+           }
+           else
+           {
+               // get current selection locations
+               previousSelection = new System.Drawing.Point(richTextBox1.SelectionStart, richTextBox1.SelectionLength);
+
+               // reset the previous word's state Colors
+               if (previousSelection != System.Drawing.Point.Empty)
+               {
+                   richTextBox1.SelectionBackColor = previousSelectionBackColor;
+               }
+
+               // save the current Selection state Colors
+               previousSelectionBackColor = richTextBox1.SelectionBackColor;
+
+               // get the location of the next word
+               var location = theWordsLocations[currentWord];
+
+               // select it
+               richTextBox1.Select(location.X, location.Y);
+
+               // highlight it
+               richTextBox1.SelectionBackColor = HighLightBackColor;
+
+               // finished ?
+               if (currentWord == nWords)
+               {
+                   richTextBox1.SelectionBackColor = previousSelectionBackColor;
+               }
+
+               // move on
+               currentWord++;
+           }
+       }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            /*  foreach (var v in voice.GetInstalledVoices().Select(v => v.VoiceInfo))
+              {
+                  Console.WriteLine("Name:{0}, Gender:{1}, Age:{2}",
+                    v.Description, v.Gender, v.Age);
+              }
+            */
+            if(comboBoxVoice.Text == "Wybierz glos")
+            {
+                MessageBox.Show("Wybierz glos");
+                return;
+            }
+            nWords = 0;
+            theWords.Clear();
+            theWordsLocations.Clear();
+            loc = 0;
+
+            //Text Selector Starts Here
+            theWords = richTextBox1.Text.Split(splitChar, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            nWords = theWords.Count;
+
+            foreach (var word in theWords)
+            {
+                theWordsLocations.Add(new System.Drawing.Point(loc, word.Length));
+                loc += word.Length + 1;
+            }
+            currentWord = 0;
+            voice.Dispose();
+            voice = new SpeechSynthesizer();
+            voice.SpeakProgress += new EventHandler<SpeakProgressEventArgs>(voice_SpeakProgress);
+            voice.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(speakCompleted);
+            voice.SetOutputToDefaultAudioDevice();
+            voice.SelectVoice(comboBoxVoice.Text);
+            voice.Rate = trackBar1.Value;
+            voice.Volume = trackBar2.Value;
+            // voice.SpeakAsyncCancelAll();
+
+      //      if (ssml)
+        //        voice.SpeakSsmlAsync(richTextBox1.Text);
+          //  else
+                voice.SpeakAsync(richTextBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            voice = new SpeechSynthesizer();
+            foreach (InstalledVoice voiceInstall in voice.GetInstalledVoices())
+            {
+                VoiceInfo infoVoice = voiceInstall.VoiceInfo;
+                comboBoxVoice.Items.Add(infoVoice.Name);
+            }
+             voice.Dispose();   
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Stream stream;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.ValidateNames = true;
+            dialog.Filter = "Txt Files|*.txt|Word Files|*.docx|PDF Files|*.pdf|SSML Files|*.ssml";
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string ext = Path.GetExtension(dialog.FileName);
+                try
+                {
+                    richTextBox1.Text = "";
+                    if (ext == ".txt")
+                        richTextBox1.LoadFile(dialog.FileName, RichTextBoxStreamType.PlainText);
+            /*        else if (ext == ".ssml")
+                    {
+                        richTextBox1.LoadFile(dialog.FileName, RichTextBoxStreamType.PlainText);
+                        ssml = true;
+                    }
+            */
+                    else if (ext == ".pdf")
+                    {
+                        loadPDF(dialog.FileName);
+                    }
+                    else if (ext == ".docx")
+                    {
+                        loadDOCX(dialog.FileName);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    
+                }
+            }
+        }
+
+        private void loadDOCX(string fileName)
+        {
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            Document doc = word.Documents.Open(fileName);
+            
+            for(int i = 1; i <= doc.Paragraphs.Count; i++)
+            {
+                richTextBox1.Text += doc.Paragraphs[i].Range.Text.Trim();
+            }
+
+            if(doc != null)
+            {
+                doc.Close();
+                Marshal.ReleaseComObject(doc);
+            }
+
+            if(word != null)
+            {
+                word.Quit();
+                Marshal.ReleaseComObject(word);
+            }
+        }
+
+        private void loadPDF(string fileName)
+        {
+            richTextBox1.Text = "";
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+            PdfDocument doc = PdfDocument.Load(fileName);
+
+            if(doc.Pages.Count <= 2)
+            {
+                foreach(var page in doc.Pages)
+                {
+                    richTextBox1.Text += page.Content.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Darmowa wersja do 2 stron");
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            voice.Pause();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            voice.Resume();
+        }
+
+        private void btnHigh_Click(object sender, EventArgs e)
+        {
+            if (comboBoxVoice.Text == "Wybierz glos")
+            {
+                MessageBox.Show("Wybierz glos");
+                return;
+            }
+
+            SpeechSynthesizer spe = new SpeechSynthesizer();
+            string sel = richTextBox1.SelectedText;
+            spe.SetOutputToDefaultAudioDevice();
+            spe.SelectVoice(comboBoxVoice.Text);
+            spe.Speak(sel.Trim());
+            spe.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SaveFileDialog dialog = new SaveFileDialog())
+                {
+                    dialog.Filter = "Wav files|*.wav";
+
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        FileStream fs = new FileStream(dialog.FileName, FileMode.Create, FileAccess.Write);
+                        voice.SetOutputToWaveStream(fs);
+                        voice.Speak(richTextBox1.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
